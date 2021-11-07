4873744dec45bb21cbdfbb659732cd2c0c6a26c3
Yana Belova <honey78444@gmail.com>
2021-11-07 14:36:13 +0300

    add FlatFactory.java file

---
+++ src/test/java/houseProject/FlatFactory.java
@@ -0,0 +1,56 @@
+package houseProject;
+
+import org.junit.Assert;
+
+public class FlatFactory {
+    final private int numberOfOneRoomFlat =1;
+    final private int numberOfTwoRoomedFlat =2;
+    final private int numberOfThreeRoomedFlat =3;
+    final private int numberOfFourRoomedFlat =1;
+    public Apartment createFlat(int numberOfRooms){
+        Apartment flat = null;
+        switch (numberOfRooms){
+            case 1:
+                System.out.println("Input square of one-room apartment: ");
+                double squareOfOneRoomFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(squareOfOneRoomFlat));
+                System.out.println("Enter the number of people who live in a one-room apartment: ");
+                int numberOfPeopleInOneRoomFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(numberOfPeopleInOneRoomFlat));
+                flat = new OneRoomFlat(numberOfOneRoomFlat, squareOfOneRoomFlat,numberOfPeopleInOneRoomFlat);
+                Assert.assertTrue("Object Apartment isn't created", Utils.objectIsCreated(flat));
+                break;
+            case 2:
+                System.out.println("Input square of two-roomed apartment: ");
+                double squareOfTwoRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(squareOfTwoRoomedFlat));
+                System.out.println("Enter the number of people who live in a two-roomed apartment: ");
+                int numberOfPeopleInTwoRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(numberOfPeopleInTwoRoomedFlat));
+                flat = new TwoRoomedFlat(numberOfTwoRoomedFlat, squareOfTwoRoomedFlat,numberOfPeopleInTwoRoomedFlat);
+                Assert.assertTrue("Object Apartment isn't created", Utils.objectIsCreated(flat));
+                break;
+            case 3:
+                System.out.println("Input square of three-roomed apartment: ");
+                double squareOfThreeRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(squareOfThreeRoomedFlat));
+                System.out.println("Enter the number of people who live in a three-roomed apartment: ");
+                int numberOfPeopleInThreeRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(numberOfPeopleInThreeRoomedFlat));
+                flat = new ThreeRoomedFlat(numberOfThreeRoomedFlat, squareOfThreeRoomedFlat, numberOfPeopleInThreeRoomedFlat);
+                Assert.assertTrue("Object Apartment isn't created", Utils.objectIsCreated(flat));
+                break;
+            case 4:
+                System.out.println("Input square of four-roomed apartment: ");
+                double squareOfFourRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(squareOfFourRoomedFlat));
+                System.out.println("Enter the number of people who live in a four-roomed apartment: ");
+                int numberOfPeopleInFourRoomedFlat = Utils.inputIntValue();
+                Assert.assertTrue("Input value should be more than 0", Utils.checkingForAPositiveValueOfAVariable(numberOfPeopleInFourRoomedFlat));
+                flat = new FourRoomedFlat(numberOfFourRoomedFlat, squareOfFourRoomedFlat,numberOfPeopleInFourRoomedFlat);
+                Assert.assertTrue("Object Apartment isn't created", Utils.objectIsCreated(flat));
+                break;
+        }
+        return flat;
+    }
+}
